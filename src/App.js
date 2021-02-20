95ea22d78e0cfa89c8be79de35417b94cb8d9727
Brando Vidal <yisusdracon@gmail.com>
2021-02-20 02:37:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { convertSheetToJson, readFileExcel } from "./service/sheet";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [file, changeFile] = useState({ data: [] });
+
+  const acceptFile = `.xlsx,.csv`;
+
+  const handleClick = () => {
+    console.log("handleClick");
+  };
+
+  const handleChange = (event) => {
+    // console.log("handleChange ", event);
+
+    const reader = readFileExcel(event);
+    reader.addEventListener("loadend", async (e) => {
+      const data = await convertSheetToJson(e);
+      console.log("readFile ", data);
+      changeFile({ data });
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+
+      <input
+        type="file"
+        name="file"
+        accept={acceptFile}
+        onChange={handleChange}
+      />
+      <button onClick={handleClick}>Read file</button>
+      <br />
+
+      {file && (
+        <div>
+          <h3>Data</h3>
+          {file.data.map((sheet) =>
+            sheet.map(({ id, page, titular, link }) => (
+              <div key={id}>
+                <p>{titular}</p>
+                <p>Pagina: {page}</p>
+                <a href={link}>{link}</a>
+              </div>
+            ))
+          )}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/service/sheet.js
@@ -0,0 +1,56 @@
+import * as ExcelJS from "exceljs";
+
+const readFileExcel = (event) => {
+  const file = event.target.files[0];
+  if (!file) {
+    return;
+  }
+  const reader = new FileReader();
+  // reader.onload = convertSheetToJson;
+  reader.readAsArrayBuffer(file);
+  return reader;
+};
+
+const convertSheetToJson = async (e) => {
+  const { result } = e.target;
+  console.info("e.target ", e.target);
+
+  const workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.load(result);
+  //   console.info("workbook ", workbook);
+
+  let worksheets = [];
+
+  workbook.eachSheet((worksheet) => {
+    // const {worksheets, } = worksheet
+    let rows = [];
+    // console.info("worksheet ", worksheet, worksheet.rowCount);
+    worksheet.eachRow((row, index) => {
+        console.info("row ", row, row.cellCount, index);
+      if (index === 1) {
+        return;
+      }
+      const content = {
+        id: getValue(row.getCell(1)),
+        medio: getValue(row.getCell(2)),
+        date: getValue(row.getCell(3)),
+        nombre: getValue(row.getCell(4)),
+        section: getValue(row.getCell(5)),
+        page: getValue(row.getCell(6)),
+        titular: getValue(row.getCell(7)),
+        link: getValue(row.getCell(8)),
+      };
+      rows = [...rows, content];
+    });
+    worksheets = [...worksheets, rows];
+  });
+  console.info("worksheets ", worksheets);
+
+  return worksheets;
+};
+
+const getValue = (cell) => {
+  return cell.value?.hyperlink || cell.value || "";
+};
+
+export { readFileExcel, convertSheetToJson };
